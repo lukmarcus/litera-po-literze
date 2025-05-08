4255e2d78b89b655a90dde8ecb8306d88f30a9ea
Marek Szumny <lukmarcus@gmail.com>
2025-05-09 00:02:46 +0200

    more changes in deploy to work with preview

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+const isPreview = process.env.PREVIEW === "true";
 const prNumber = process.env.PR_NUMBER;
 
 export default defineConfig({
-  base: prNumber ? `/litera-po-literze/pr-${prNumber}/` : "/litera-po-literze/",
+  base: isPreview
+    ? `/litera-po-literze/pr-${prNumber}/`
+    : "/litera-po-literze/",
   plugins: [react()],
   server: {
     open: true, // Automatically open the app in the browser
