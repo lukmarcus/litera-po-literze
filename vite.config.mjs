c6f0a8bfc2c2d72e5b290aca4f57f16b25176625
Marek Szumny <lukmarcus@gmail.com>
2025-05-08 21:58:34 +0200

    vite in deploy changed

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+const prNumber = process.env.PR_NUMBER;
+
 export default defineConfig({
+  base: prNumber ? `/litera-po-literze/pr-${prNumber}/` : "/",
   plugins: [react()],
   server: {
     open: true, // Automatically open the app in the browser
