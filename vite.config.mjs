6cc3e91ff1e8d2d3b9a45d77f59d8d43a5582d9e
Marek Szumny <lukmarcus@gmail.com>
2025-05-25 00:52:50 +0200

    preview deploy fixes 4

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,22 +4,17 @@ const isPreview = process.env.PREVIEW === "true";
 const prNumber = process.env.PR_NUMBER;
 
 export default defineConfig({
-  base: isPreview
-    ? `/litera-po-literze/pr-${prNumber}/`
-    : "/litera-po-literze/",
+  base:
+    isPreview && prNumber
+      ? `/litera-po-literze/pr-${prNumber}/`
+      : "/litera-po-literze/",
   build: {
     outDir: "dist",
     rollupOptions: {
       output: {
-        entryFileNames: isPreview
-          ? `assets/[name]-[hash].js`
-          : "assets/[name]-[hash].js",
-        chunkFileNames: isPreview
-          ? `assets/[name]-[hash].js`
-          : "assets/[name]-[hash].js",
-        assetFileNames: isPreview
-          ? `assets/[name]-[hash][extname]`
-          : "assets/[name]-[hash][extname]",
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash][extname]`,
       },
     },
   },
