928ef8965aa67ce885e8d8b1c1fc55caaaadbebb
Marek Szumny <lukmarcus@gmail.com>
2025-07-04 17:03:45 +0200

    new English pack with 2-letter words

--- src/wordPacks/en.ts
+++ src/wordPacks/en.ts
@@ -1,7 +1,14 @@
 import { WordPack } from "../types/wordPack";
+import { EN02 } from "../data/en02";
 import { EN99 } from "../data/en99";
 
 export const enWordPacks: WordPack[] = [
+  {
+    id: "en02",
+    name: "English 2-letter words",
+    type: "basic",
+    words: EN02.map(({ en }) => ({ word: en })),
+  },
   {
     id: "en99",
     name: "English basic pack",
